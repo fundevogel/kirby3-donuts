315914a93a7f43a02fac02ae3f620d95b0e39cf7
Martin Folkers <hello@twobrain.io>
2020-08-09 23:09:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# kirby3-donuts
+[![Release](https://img.shields.io/github/release/Fundevogel/kirby3-donuts.svg)](https://github.com/Fundevogel/kirby3-donuts/releases) [![License](https://img.shields.io/github/license/Fundevogel/kirby3-donuts.svg)](https://github.com/Fundevogel/kirby3-donuts/blob/master/LICENSE) [![Issues](https://img.shields.io/github/issues/Fundevogel/kirby3-donuts.svg)](https://github.com/Fundevogel/kirby3-donuts/issues) [![Status](https://travis-ci.org/fundevogel/kirby3-donuts.svg?branch=master)](https://travis-ci.org/fundevogel/kirby3-donuts)
+
+A Kirby v3 plugin for creating SVG donut (and pie) charts.
+
+
+## What
+This library is a Kirby v3 wrapper for the PHP port [`tiny-phpeanuts`](https://github.com/Fundevogel/tiny-phpeanuts) of the TypeScript library [`tiny-donuts`](https://github.com/Verivox/tiny-donuts).
+
+
+## Roadmap
+- [ ] Add tests
+- [ ] Add pie charts
+- [ ] Add size adjustments
+
+## Credits
+Naturally, a big shoutout goes to [Kim Almasan](https://github.com/Narquadah) & [Lars Krumbier](), who created `tiny-donuts` for [Verivox GmbH](https://github.com/Verivox). Most of the helper functions were taken from [Kirby](https://getkirby.com) by [Bastian Allgeier](https://github.com/bastianallgeier) (who's just awesome, btw).
+
+
+**Happy coding!**
+
+
+:copyright: Fundevogel Kinder- und Jugendbuchhandlung
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+// @include_once __DIR__ . '/vendor/autoload.php';
+
+use Fundevogel\Chart;
+
+Kirby::plugin('fundevogel/donuts', [
+    'options' => [
+        'thickness' => 3,
+        'spacing' => 0.005,
+        'size' => 100,
+        'inline' => false,
+        'template' => 'donut',
+    ],
+    'blueprints' => [
+        'fields/donut' => __DIR__ . '/blueprints/field.yml',
+        'files/donut' => __DIR__ . '/blueprints/file.yml',
+    ],
+    'pageMethods' => [
+        /**
+         * @param array $entries
+         * @param float $thickness
+         * @param float $spacing
+         * @return Kirby\Cms\File|string
+         */
+        'toDonut' => function (
+            array $entries,
+            float $thickness = null,
+            float $spacing = null
+        ) {
+            try {
+                $file = (new Chart($this, [
+                    'entries' => $entries,
+                    'thickness' => $thickness,
+                    'spacing' => $spacing,
+                ]))->render();
+            } catch (Exception $e) {
+                throw $e;
+            }
+
+            return $file;
+        }
+    ],
+    'fieldMethods' => [
+        /**
+         * @param Kirby\Cms\Field $field
+         * @param float $thickness
+         * @param float $spacing
+         * @return Kirby\Cms\File|string
+         */
+        'toDonut' => function (
+            Kirby\Cms\Field $field,
+            float $thickness = null,
+            float $spacing = null
+        ) {
+            $page = $field->model();
+            $entries = $field->toStructure()->toArray();
+
+            try {
+                $file = (new Chart($page, [
+                    'entries' => $entries,
+                    'thickness' => $thickness,
+                    'spacing' => $spacing,
+                ]))->render();
+            } catch (Exception $e) {
+                throw $e;
+            }
+
+            return $file;
+        }
+    ]
+]);
